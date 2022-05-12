cd8a3b6cf863c6705bcc20db919d86c959aaf85b
palegreramos <palegre@iespuntadelverde.es>
2022-05-12 19:01:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-n6nchf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-n6nchf)
---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+interface Wine {
+  name: string;
+  price: number;
+  imageUrl: string;
+  onSale: boolean;
+}
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  public wine: Wine;
+  public message: string = '';
+
+  constructor() {}
+
+  createWine(wineForm) {
+    if (wineForm.invalid) {
+      this.message = 'Please correct all errors and resubmit the form';
+    } else {
+      this.message = '';
+      this.wine = wineForm.value.wine;
+      console.log('Creating wine', this.wine);
+    }
+  }
+}
